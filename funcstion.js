1e9efb3720b378183fdf9754ccd02e53cb57a6b0
jahidhasan6676 <jh18186676@gmail.com>
2024-09-07 10:29:43 +0600

    added the function file

---
+++ funcstion.js
@@ -0,0 +1,369 @@
+// const number = 20;
+
+// function fanOffKor(){
+//     console.log('fan off koro')
+// }
+// fanOffKor();
+// fanOffKor();
+
+// function jahid(){
+//     console.log('my name is jahid hasan');
+// }
+// jahid();
+
+// function square (code){
+//     console.log(code)
+//     const result = code * code;
+//     console.log('square of the number is:', result);
+  
+// }
+
+// square(4);
+// square(21);
+// square(33);
+
+// function square(number){
+//     console.log(number);
+//     const borgo = number * number;
+//     console.log('borgo number is:', borgo);
+// }
+// square(5);
+// square(10);
+// const total = square(12);
+// console.log(total);
+
+// function add(num1, num2, num3) {
+//     const sum = num1 + num2 + num3;
+//     console.log(sum);
+// }
+// add(2, 3, 5, );
+// add(4, 56, 65, );
+
+// function add(num1, num2, num3, num4){
+//     const sum = num1 + num2 + num3 + num4;
+//     console.log(sum);
+    
+// }
+//  add(5, 10, 8, 5);
+//  add(10, 20, 30, 40,);
+
+
+
+// function double(number){
+//     const doubled = number * 2;
+//     console.log(number, doubled);
+// }
+// const number = 56;
+// double(number)
+// double(14);
+// double(20);
+// double(34);
+
+// function double(number){
+//     const total = number * 2;
+//     console.log(total);
+
+// }
+//   const number = 30;
+//   double(number);
+
+
+
+// function difference(num1, num2){
+//     const result = num1 - num2;
+//     console.log(num1, '-', num2, ':', result);
+// }
+// const age = 23;
+// const brotherAge = 15;
+// difference(age, brotherAge)
+
+// function mainus(num1, num2){
+//     const diff = num1 - num2;
+//     console.log(diff);
+// }
+// const price = 20;
+// const take = 10;
+// mainus(price, take)
+
+// function tenTimes(number){
+//     const result = number * 10;
+//     return result;
+    
+// }
+
+// const output = tenTimes(10);
+// console.log('output', ':', output);
+// const multiple = tenTimes(90);
+// console.log('output', ':', multiple)
+
+// function add(num1, num2, num3) {
+//         const sum = num1 + num2 + num3;
+//         return sum;
+//     }
+//     const total = add(20, 30, 10);
+//     console.log(total);
+
+// function add(num1, num2, num3) {
+//         return num1 + num2 + num3;
+   
+// }
+// const total = add(20, 30, 10);
+// console.log(total);
+
+// function add(num1, num2){
+//     const total = num1 + num2;
+//     return total;
+// }
+// const bill = add(20, 40);
+// console.log(bill)
+
+// function add2(price1, price2){
+//     return price1 + price2;
+// }
+// const billed = add2(20, 40);
+// console.log(billed)
+
+
+// function doMath(num1, num2){
+//     const sum = num1 + num2;
+//     const diff = num1 - num2;
+//     const multiply = sum * diff;
+//     const result = multiply / 2;
+//     return result;
+// }
+// const result  = doMath(20, 5);
+// console.log(result)
+
+// function doMath(num1, num2){
+//     const sum = num1 + num2;
+//     const diff = num1 - num2;
+//     const multiply = sum * diff;
+//     const divide = multiply / 2;
+//     return diff;
+// }
+// const total = doMath(20, 10);
+// console.log(total)
+
+// function isEven(number){
+//     if(number % 2 === 0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// console.log(isEven(10))
+
+// function isEven(number){
+//     if(number % 2 === 0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+//     console.log(isEven(10));
+//     const total = isEven(11);
+//     console.log('this is even number is:', total);
+
+// function isOdd(number){
+//     if(number % 2 === 1){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// const odds = isOdd(10)
+// console.log(odds)
+
+
+// function evenString(str){
+//     const size = str.length;
+//     console.log(str, size);
+//     if(size % 2 === 0){
+//         console.log('even size');
+
+//     }
+//     else{
+//         console.log('odd size');
+//     }
+// }
+// evenString('cumilla');
+// evenString('rajshahi')
+
+// function oddEven(str){
+//     const num = str.length;
+//     console.log(num);
+//     if(num % 2 === 0){
+//         console.log('even number');
+//     }
+//     else{
+//         console.log('odd number')
+//     }
+// }
+// oddEven('borishal');
+// oddEven('cumilla');
+
+
+// function doubleOrTriple(double, triple){
+//     if(triple === true){
+//         const doubled = double * 3;
+//         return doubled;
+//     }
+//     else{
+//        const doubled = double * 2;
+//        return doubled;
+//     }
+// }
+//     console.log(doubleOrTriple(5, true))
+//     console.log(doubleOrTriple(10, true))
+//     console.log(doubleOrTriple(8, false))
+
+// function numberArray(number){
+//     const len = number.length;
+//     return len;
+// }
+// const len = numberArray([12, 32, 43, 45]);
+// console.log(len)
+
+// function sumNumber(numbers){
+//     let sum = 0;
+//     for(const number of numbers){
+//         console.log(number);
+//         sum = sum + number;
+//     }
+//     return sum;
+
+// }
+// const nums = [12, 23, 45];
+// const sum = sumNumber(nums);
+// console.log('sum of number is', sum)
+
+// function sumNumber(numbers){
+//     let sum = 0;
+//     for(let number of numbers){
+//         console.log(number);
+//         sum = sum + number;
+        
+//     }
+//     return sum;
+// }
+// const sum = sumNumber([2, 34, 45, 54, 56, 67,]);
+// console.log('even number of sum is:', sum);
+
+
+// function evenNumber(numbers){
+//     let sum = 0;
+//     for(const number of numbers){
+//         if(number % 2 === 0){
+//             console.log(number);
+//             sum = sum + number;
+//             // console.log(sum);
+//         }
+//     }
+//     return sum;
+// }
+// const numbers = [12, 23, 45, 56, 58];
+// const even = evenNumber(numbers);
+// console.log('even of number is', even)
+
+
+// function evenNumber(numbers){
+//     let sum = [];
+//     for(const number of numbers){
+//         if(number % 2 === 0){
+//             console.log(number);
+//             sum.push(number);
+            
+            
+//         }
+//     }
+//     return sum;
+// }
+// const numbers = [12, 23, 45, 56, 58];
+// const even = evenNumber(numbers);
+// console.log('even of number is', even)
+
+// function evenNumber(numbers){
+//     let sum = [];
+//    for(let number of numbers){
+    
+//     if(number % 2 === 0){
+//         console.log(number);
+//         sum.push(number);
+//     }
+//     }
+//     return sum;
+    
+// }
+// const num = [12, 34, 24, 55, 67];
+// const evens = evenNumber(num);
+// console.log(evens);
+
+// function multiply(a, b, c, d){
+//     const multi = a * b * c * d;
+//     return multi;
+    
+// }
+// const total = (multiply(2, 3, 4, 5));
+// console.log(total);
+
+// function double(number){
+//     if(number % 2 === 0){
+//         return number / 2;
+//     }
+//     else{
+//         return number * 2;
+//     }
+// }
+// const total = double(11);
+// console.log(total);
+
+// function make_avg(a, b, c, d, e){
+//     const sum = a + b + c + d + e;
+//     // console.log(sum);
+//     const avarage = sum / 5;
+//    console.log(avarage);
+//     return avarage;
+// }
+// const total = make_avg(5, 3, 6, 4, 2);
+// console.log(total);
+
+
+function count_zero(binaryString) {
+    
+    
+    for (let i = 0; i < binaryString; i++) {
+        if (binaryString === '0') {
+    
+        }
+    }
+    
+    return ;
+}
+
+// Example usage:
+const binaryStr = count_zero ("11010100010");
+console.log(binaryStr);  // Output: 6
+
+
+
+
+// function count_zero(binaryString) {
+//     let count = 0;
+    
+//     for (let i = 0; i < binaryString.length; i++) {
+//         if (binaryString[i] === '0') {
+//             count++;
+//         }
+//     }
+    
+//     return count;
+// }
+
+// // Example usage:
+// const binaryStr = "11010100010";
+// console.log(count_zero(binaryStr));  // Output: 6
+
+
